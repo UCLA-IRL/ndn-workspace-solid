240bde024727c0d2e475f6cc1b13e30e3f10cc85
Xinyu Ma <ma.xinyu.26a@kyoto-u.jp>
2024-03-10 18:53:07 -0700

    Add debug features

--- src/components/config/config-page.tsx
+++ src/components/config/config-page.tsx
@@ -1,31 +1,18 @@
-import { onMount, createSignal } from 'solid-js'
-import { REVISION, TIMESTAMP } from '../../build-meta'
+// import { onMount, createSignal } from 'solid-js'
+import { Stack } from '@suid/material'
+import VersionTable from './version-table'
+import WorkspaceState from './workspace-state'
+import UpdateInspect from './update-inspect'
 
 export default function ConfigPage() {
-  const [rev, setRev] = createSignal<string>()
-  const [times, setTimes] = createSignal<number>()
-
-  onMount(async () => {
-    try {
-      const response = await fetch(`${location.origin}/build-meta.json`)
-      const json = await response.json()
-      setRev(json['revision'])
-      setTimes(json['timestamp'])
-    } catch {
-      // Ignore errors
-    }
-  })
-
   return (
     <>
-      <p>Versions</p>
-      <p>
-        Running build: <code>{REVISION}</code> {new Date(TIMESTAMP * 1000).toLocaleString()}
-      </p>
-      <p>
-        Remote build: <code>{rev() ?? 'UNKNOWN'}</code>{' '}
-        {times() ? new Date(times()! * 1000).toLocaleString() : 'UNKNOWN'}
-      </p>
+      <h1>DEBUG PAGE</h1>
+      <Stack direction="column" spacing={2} justifyContent="flex-start">
+        <VersionTable />
+        <WorkspaceState />
+        <UpdateInspect />
+      </Stack>
     </>
   )
 }
