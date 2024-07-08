c3b5259d4380668699d561fa940b773b14c2b138
Ananya Chavadhal <125298075+ananyac9@users.noreply.github.com>
2024-07-08 22:41:22 +0530

    ADD: instructions to generate trust anchor and safebag on workspace page

--- src/components/workspace/app-namespace.tsx
+++ src/components/workspace/app-namespace.tsx
@@ -119,6 +119,9 @@ export default function AppNamespace(props: {
             <Typography color="secondary" component={'span'}>
               Please input the trust anchor exported by cert-dump
             </Typography>
+            <Typography color="secondary" component={'p'}>
+              After following instructions on <a href="/">home page</a>, this can be obtained by: <code>ndnsec cert-dump -i /my-workspace</code>
+            </Typography>
             <QrReader popupOpen={isPopupOpen()} setValue={setValue} />
           </Show>
         }
