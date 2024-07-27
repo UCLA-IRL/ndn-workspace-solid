f0d465091f33726bf6f1be73a7e2fbc0685e108b
Sinclair19 <zsq0219@outlook.com>
2024-07-26 22:59:49 -0700

    build: minor fix fix `./build-meta.bash: 7: [[: not found` fix build-meta.ts generate syntax problem

--- build-meta.bash
+++ build-meta.bash
@@ -1,7 +1,8 @@
+#!/bin/bash
 revision="$(git describe --tags --abbrev=0) $(git rev-parse --short HEAD)"
 timestamp=$(date +%s)
 echo '{"revision":"'${revision}'","timestamp":'${timestamp}'}' > ./public/build-meta.json
-echo 'export const REVISION = '"'"${revision}"'" \
+echo -e 'export const REVISION = '"'"${revision}"'" \
   '\n// eslint-disable-next-line @typescript-eslint/no-loss-of-precision, prettier/prettier' \
   '\nexport const TIMESTAMP = '${timestamp} > ./src/build-meta.ts
 if [[ $SHELL == *"zsh" ]]; then
