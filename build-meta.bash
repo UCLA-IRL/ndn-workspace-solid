6a4e0d59ae151922df6ffe359463277869abb5d5
zjkmxy <kinu@meta.com>
2024-08-09 14:23:40 -0700

    Update build-meta.bash for sed

--- build-meta.bash
+++ build-meta.bash
@@ -5,8 +5,4 @@ echo '{"revision":"'${revision}'","timestamp":'${timestamp}'}' > ./public/build-
 echo -e 'export const REVISION = '"'"${revision}"'" \
   '\n// eslint-disable-next-line @typescript-eslint/no-loss-of-precision, prettier/prettier' \
   '\nexport const TIMESTAMP = '${timestamp} > ./src/build-meta.ts
-if [[ $SHELL == *"zsh" ]]; then
-  sed -i -r 's/\([^ \t]+\)[ \t]+$/\1/' ./src/build-meta.ts
-else
-  sed -i -r 's/([^ \t]+)[ \t]+$/\1/' ./src/build-meta.ts
-fi
+sed -i -r 's/([^ \t]+)[ \t]+$/\1/' ./src/build-meta.ts
--- src/build-meta.ts
+++ src/build-meta.ts
@@ -1,3 +1,3 @@
-export const REVISION = 'v1.2.3 fbea0c5'
-// eslint-disable-next-line @typescript-eslint/no-loss-of-precision, prettier/prettier
-export const TIMESTAMP = 1721591925
+export const REVISION = 'v1.2.3 639e4dd' 
+// eslint-disable-next-line @typescript-eslint/no-loss-of-precision, prettier/prettier 
+export const TIMESTAMP = 1723237205
