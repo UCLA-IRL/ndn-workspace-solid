00ab58243465c00938ba4c246de7601e2a6167b2
Xinyu Ma <kinu@meta.com>
2024-09-01 00:42:40 -0700

    Fix update inspector

--- src/build-meta.ts
+++ src/build-meta.ts
@@ -1,3 +1,3 @@
-export const REVISION = 'v1.2.9 4b53452'
+export const REVISION = 'v1.2.9 8cf4914'
 // eslint-disable-next-line @typescript-eslint/no-loss-of-precision, prettier/prettier
-export const TIMESTAMP = 1723587629
+export const TIMESTAMP = 1725176429
