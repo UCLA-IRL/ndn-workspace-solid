174c7cafc6f28e642980c4fcdd5d1c54bf6ff238
Sirapop Theeranantachai <sirapop741@gmail.com>
2025-02-22 05:07:54 +0000

    sass: upgrade .scss to @forward tailwindcss instead of @import

--- src/build-meta.ts
+++ src/build-meta.ts
@@ -1,3 +1,3 @@
-export const REVISION = 'v1.3.8 ecaff75'
+export const REVISION = ' f2bdbb4'
 // eslint-disable-next-line @typescript-eslint/no-loss-of-precision, prettier/prettier
-export const TIMESTAMP = 1739751831
+export const TIMESTAMP = 1740200769
