b4c6fb5cac1319e8028a516cd05fb34f87baa9c7
Sirapop Theeranantachai <sirapop741@gmail.com>
2025-02-22 14:44:58 -0800

    markdown: fixed the yjs cursor

--- src/build-meta.ts
+++ src/build-meta.ts
@@ -1,3 +1,3 @@
-export const REVISION = ' 174c7ca'
+export const REVISION = ' c8f5119'
 // eslint-disable-next-line @typescript-eslint/no-loss-of-precision, prettier/prettier
-export const TIMESTAMP = 1740210461
+export const TIMESTAMP = 1740264206
