5f547d89162d2d352fe224c8661fed81c0b5d389
jaczhi <54284925+jaczhi@users.noreply.github.com>
2025-02-26 10:21:04 -0800

    use local storage instead of cookie

--- src/components/chat/chat-state-store.tsx
+++ src/components/chat/chat-state-store.tsx
@@ -1,14 +1,13 @@
-import Cookies from "js-cookie";
 import { createSignal } from "solid-js";
 
-export function useChatState<T>(cookieName: string, defaultValue: T) {
+export function useChatState<T>(keyName: string, defaultValue: T) {
   const [chatState, setChatState] = createSignal<T>(
-    JSON.parse(Cookies.get(cookieName) || JSON.stringify(defaultValue))
+    JSON.parse(localStorage.getItem(keyName) || JSON.stringify(defaultValue))
   );
 
   const updateChatState = (newValue: T) => {
     setChatState(() => newValue);
-    Cookies.set(cookieName, JSON.stringify(newValue), { expires: 365 });
+    localStorage.setItem(keyName, JSON.stringify(newValue));
   };
 
   return { chatState, updateChatState };
